8feba04bb86186d4425331cda6dca075435b5a6e
Fidel <fidel.valdes@gmail.com>
2022-03-02 22:53:51 -0300

    Primer Commit Mi Portafolio

---
+++ asset/Js/Miportafolio.js
@@ -0,0 +1,27 @@
+subone (confirmado,filo)
+fatkais(confirmado,filo)
+alieligena
+mitggesh
+chico zeus
+chyste
+alimayeli
+rapdrina
+sabotage
+millaray
+mente inquieta
+nebler
+los mister bailarines
+tecnica clave
+stailok
+big suko
+graffi
+conexion de barrio
+ripsantino (por confirmar,filo)
+cronel negro(por confirmar, filo)
+solo pablish
+rigiri clan
+lucero
+boomer
+adicta sinfonia
+las dilas
+mister daniel
